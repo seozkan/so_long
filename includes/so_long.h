72c4770feec4d52f79d6524f4964d869f3061e22
Seçkin Ersin Özkan <ozkan003@gmail.com>
2023-03-09 18:54:33 +0300

    v2.2

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: seozkan <seozkan@student.42kocaeli.com.tr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 16:13:49 by seozkan           #+#    #+#             */
-/*   Updated: 2023/03/09 18:30:39 by seozkan          ###   ########.fr       */
+/*   Updated: 2023/03/09 18:54:19 by seozkan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,10 @@
 # include <fcntl.h>
 
 # define K_ESC 65307
-# define K_UP 65362
-# define K_DOWN 65364
-# define K_LEFT 65361
-# define K_RIGHT 65363
-# define K_A 97
-# define K_W 119
-# define K_S 115
-# define K_D 100
+# define K_UP 119
+# define K_DOWN 115
+# define K_LEFT 97
+# define K_RIGHT 100
 
 # define E_MALLOC "Failed to allocate memory!"
 # define E_ARG "The program must be run with a map argument!"
