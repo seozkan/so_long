edaa4cab315b1ec18b52d364d1eefb31cfe664f4
seozkan <ozkan003@gmail.com>
2023-03-11 15:07:09 +0300

    v3.0

---
+++ lib/ft_printf/ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seozkan <seozkan@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 21:24:37 by seozkan           #+#    #+#             */
+/*   Updated: 2022/12/24 17:30:19 by seozkan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *str, ...);
+#endif
